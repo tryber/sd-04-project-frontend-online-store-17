0cacea06474a4d74eb169cd331c5308801d6e583
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-18 17:12:12 -0300

    colocando header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
+import SearchInput from './components/header'
 
 function App() {
   return (
     <div className="App">
       <div>
         <BrowserRouter>
+        <SearchInput />
           <Switch>
             <Route path="/" component={ProductList} />
           </Switch>
