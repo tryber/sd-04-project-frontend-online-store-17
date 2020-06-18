55c39c26cd1a75e290d279ef68b3fd2d6be5159e
cacobribeiro <cacobribeiro@gmail.com>
2020-06-18 15:23:17 -0300

    Renderizando as categorias

--- src/pages/SideBar.js
+++ src/pages/SideBar.js
@@ -1,12 +1,36 @@
 import React from 'react';
+import * as api from '../services/api';
 
 export default class SideBar extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      categories: '',
+      loaded: false,
+    };
+  }
+
+  componentDidMount() {
+    api.getCategories().then((data) => this.setState({ categories: data, loaded: true }));
+  }
+
+  onClickBtn(event) {
+    console.log(event);
+  }
+
   render() {
-    const { categories } = this.props;
+    const { categories } = this.state;
+    console.log(categories);
+    if (this.state.loaded === false) return <div>Oi</div>;
     return (
-      <div>
-        <div>
-          <p>Categorias:</p>
+      <div data-testid="side-bar">
+        <div data-testid="btn-categorie">
+          {categories.map((e) => (
+            <button onClick={(e) => this.onClickBtn(e)} key={e.id}>
+              {e.name}
+            </button>
+          ))}
         </div>
       </div>
     );
