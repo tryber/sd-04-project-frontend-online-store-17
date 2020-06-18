947a1794dadabe046c2bc275f316fd4bcac8c21c
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-18 17:17:58 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -2,14 +2,14 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
-import SearchInput from './components/header'
+import SearchInput from './components/header';
 
 function App() {
   return (
     <div className="App">
       <div>
         <BrowserRouter>
-        <SearchInput />
+          <SearchInput />
           <Switch>
             <Route path="/" component={ProductList} />
           </Switch>
--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,20 @@ import carrinho from '../image/carrinho.png';
 class SearchInput extends React.Component {
   render() {
     return (
-    <div>
       <div>
-        <label htmlFor='labelInput'>Pesquisa</label>
-        <input
-          type='text'
-          name='labelInput'
-          id='labelInput'
+        <div>
+          <label htmlFor="labelInput">Pesquisa</label>
+          <input
+            type="text"
+            name="labelInput"
+            id="labelInput"
           />
+        </div>
+        <div>
+          <img src={carrinho} alt="imagem carrinho" />
+        </div>
       </div>
-      <div>
-        <img src={carrinho} alt='imagem carrinho' />
-      </div>
-    </div>
-    )}
+    ) };
 }
 
 
