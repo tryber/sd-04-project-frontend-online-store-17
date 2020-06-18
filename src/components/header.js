8a1a33406a94a97db44d581a9cd7dbff0e74dec4
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-18 16:36:57 -0300

    criando header

---
+++ src/components/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import carrinho from '../image/carrinho.png';
+
+class SearchInput extends React.Component {
+  render() {
+    return (
+    <div>
+      <div>
+        <label htmlFor='labelInput'>Pesquisa</label>
+        <input
+          type='text'
+          name='labelInput'
+          id='labelInput'
+          />
+      </div>
+      <div>
+        <img src={carrinho} alt='imagem carrinho' />
+      </div>
+    </div>
+    )}
+}
+
+
+export default SearchInput;
