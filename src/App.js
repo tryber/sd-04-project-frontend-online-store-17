d045cc9ecc2c9ecbd108a6f4c9f952f73db3b602
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-19 15:03:45 -0300

    cc

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <BrowserRouter>          
+        <BrowserRouter>
           <ShoppingCartButton />
           <Switch>
             <Route path="/" component={ProductList} />
