2694edfba7fe1c3329334d4dc814c25ea7c8aba9
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-19 16:46:37 -0300

    trocando string por state

--- src/pages/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage.js
@@ -4,13 +4,13 @@ class ShoppingCartPage extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = "Carrinho vazio";
+    this.state = "Seu carrinho está vazio";
   }
 
   render() {
     return (
       <div data-testid="shopping-cart-empty-message">
-        <span data-testid="shopping-cart-empty-message">Seu carrinho está vazio</span>
+        <span data-testid="shopping-cart-empty-message">{this.state}</span>
       </div>
     );
   }
