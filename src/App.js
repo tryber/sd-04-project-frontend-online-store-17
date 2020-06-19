727db9b89803444c6077258a64ceba128f494298
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-19 14:58:59 -0300

    tirando barra de pesquisa

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import React from 'react';
 import './App.css';
 import ProductList from './components/ProductList';
-import SearchInput from './components/header';
 import ShoppingCartPage from './pages/ShoppingCartPage';
 import ShoppingCartButton from './components/shoppingCartButton';
 
@@ -10,8 +9,7 @@ function App() {
   return (
     <div className="App">
       <div>
-        <BrowserRouter>
-          <SearchInput />
+        <BrowserRouter>          
           <ShoppingCartButton />
           <Switch>
             <Route path="/" component={ProductList} />
