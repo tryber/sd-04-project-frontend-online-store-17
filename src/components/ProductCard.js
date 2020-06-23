0cdaa1da479d625ac858225a52f3e2c00718dbd7
Julio Cesar <stininmatic@hotmail.com>
2020-06-23 00:23:48 -0300

    Componentes falhados

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -10,8 +10,8 @@ export default class ProductCard extends React.Component {
         <img src={thumbnail} alt={id} />
         <p>{title}</p>
         <p>{`R$ ${price}`}</p>
-        <Link data-testid="product-detail-link" to={{ pathname: `/${id}`, product }}>
-          )>DETALHES
+        <Link data-testid="product-detail-link" to={{ pathname: `/product/${id}/detail`, product }}>
+          DETALHES
         </Link>
       </div>
     );
