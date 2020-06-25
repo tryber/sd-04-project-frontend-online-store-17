8b8d807a646ff3dd54e3a7d9b92fb9b3bde8d6ff
Ronald Lima <ronaldazevedolima@gmail.com>
2020-06-25 18:59:27 -0300

    cc5

--- src/pages/ShoppingCartPage.js
+++ src/pages/ShoppingCartPage.js
@@ -17,40 +17,36 @@ class ShoppingCartPage extends React.Component {
   }
 
   getStorage() {
-    if(!localStorage.lista) localStorage.lista = JSON.stringify([])
+    if (!localStorage.lista) localStorage.lista = JSON.stringify([]);
     const lista = JSON.parse(localStorage.getItem('lista'));
     this.setState({ cartItens: lista });
-  };
+  }
 
   filtrar(elemento) {
     const carrinho = this.state.cartItens;
-    return carrinho.filter((ele) => ele.title == elemento.title).length;
-  };
+    return carrinho.filter((ele) => ele.title === elemento.title).length;
+  }
 
   renderizar() {
     const product = this.state.cartItens;
-    if (this.state.cartItens.length === 0)
+    if (this.state.cartItens.length === 0) {
       return (
-        <span data-testid='shopping-cart-empty-message'>
+        <span data-testid="shopping-cart-empty-message">
           Seu carrinho está vazio
         </span>
-      );
+      );}
     return (
       <div>
         <h2>Carrinho de compras</h2>
         {product.map((e) => (
           <div key={e.id}>
             <img src={e.thumbnail} alt={e.id} />
-            <p data-testid='shopping-cart-product-name'>{e.title}</p>
-            <p data-testid='shopping-cart-product-quantity'>
-              {this.filtrar(e)}
-            </p>
+            <p data-testid="shopping-cart-product-name">{e.title}</p>
+            <p data-testid="shopping-cart-product-quantity">{this.filtrar(e)}</p>
             <p>{`R$ ${e.price}`}</p>
             <Link
-              data-testid='product-detail-link'
-              to={{ pathname: `/product/${e.id}/detail`, e }}
-            >
-              DETALHES
+              data-testid="product-detail-link"
+              to={{ pathname: `/product/${e.id}/detail`, e }}>DETALHES
             </Link>
           </div>
         ))}
@@ -63,7 +59,7 @@ class ShoppingCartPage extends React.Component {
   };
 
   render() {
-    return <div>{this.renderizar()}</div>;
+    return <div>{this.renderizar()}</div>
   }
 }
 
